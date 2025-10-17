c6d10b90717be136cf6e154ff7b9791e42fcd750
pinkLemon3 <meriblu1211@gmail.com>
2025-10-17 21:58:25 +0100

    create function to get the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Start coding the game!')
+// console.log('Start coding the game!')- 
+
+
+// Getting a random number to attach to one of the 3 choices
+
+let randomNumber = Math.floor(Math.random() * 3);
+
+let computerChoice;                                // Declare the variable
+function getComputerChoice() {
+if(randomNumber === 0) {
+    console.log("rock");
+}else if(randomNumber === 1) {
+    console.log("paper");
+}else {
+    console.log("scissors")
+}
+}
+getComputerChoice();
