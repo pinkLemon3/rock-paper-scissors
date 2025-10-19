0e644ada517194e276735f558d143a6abbc8146d
pinkLemon3 <meriblu1211@gmail.com>
2025-10-19 21:54:50 +0100

    Create function playGame() include playRound() and use for loop to iterate the game 5 times log the scores in the console

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // console.log('Start coding the game!')- Check if JavaScript is linked correctly
 
 // Create the variables for keeping score
-let humanScore = 0;
-let computerScore = 0;
-
 
 // Declare the variable
 // Getting a random number to attach to one of the 3 choices
@@ -21,7 +18,7 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 // Get human choice 
 //Declare the variable that is holding the human response
@@ -36,28 +33,56 @@ function getHumanChoice() {
 // Write the logic to play a single round and increment the score for the winner
 // Asign the choices of the 2 players to 2 global variables that we will use as parameters for the playRound(). 
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+//playRound(humanSelection, computerSelection);
 
 // Write function that plays one round and lets the players see who won. 
 // Increment the score for the winner.
 
-function playRound(humanSelection, computerSelection) {
-    if (humanSelection === computerSelection) {
-        console.log(`It's a tie! You both picked ${humanSelection}.`);
-    } else if (
-        (humanSelection === "rock" && computerSelection === "scissors") ||
-        (humanSelection === "paper" && computerSelection === "rock") ||
-        (humanSelection === "scissors" && computerSelection === "paper")) {
-        console.log(`Congratulations, you win! ${humanSelection} beats ${computerSelection}.`);
-        humanScore++;
+// Write the logic to play the entire game
+// Use a for loop to iterate the game 5 times
+
+// Declare 2 global variables for keeping the overall score after 5 games
+let overallHumanScore = 0;
+let overallComputerScore = 0;
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanSelection, computerSelection) {
+        if (humanSelection === computerSelection) {
+            console.log(`It's a tie! You both picked ${humanSelection}.`);
+        } else if (
+            (humanSelection === "rock" && computerSelection === "scissors") ||
+            (humanSelection === "paper" && computerSelection === "rock") ||
+            (humanSelection === "scissors" && computerSelection === "paper")) {
+            console.log(`Congratulations, you win! ${humanSelection} beats ${computerSelection}.`);
+            humanScore++;
+        } else {
+            console.log(`Sorry, you lost! ${computerSelection} beats ${humanSelection}.`);
+            computerScore++;
+        }
+    }
+    //Use a for loop that calls the function 5 times, each with a different choice result
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`Score after round ${i + 1}: You ${humanScore} - ${computerScore} Computer\n`);
+    }
+    // Determine the winner 
+    if (humanScore > computerScore) {
+        console.log(`You win the game! Final score: You ${humanScore}- ${computerScore} Computer`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lose the game! Final score: You ${humanScore}- ${computerScore} Computer`);
     } else {
-        console.log(`Sorry, you lost! ${computerSelection} beats ${humanSelection}.`);
-        computerScore++;
+        console.log(`The game is a tie! Final score:  You ${humanScore}- ${computerScore} Computer`);
     }
 }
 
-playRound(humanSelection, computerSelection);
+playGame();
+
+
 
 
 
